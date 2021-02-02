9b8ed64fecf5ad9269b3f8e42ac8ff1a1efc6332
Muhammad Adib Ahmad Roslan <adbroslan@gmail.com>
2021-02-02 09:44:33 +0800

    added policy

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        // return true;
+        return auth()->user()->can('view-all-user');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function view(User $user, User $model)
+    {
+        return auth()->user()->can('view-user');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return auth()->user()->can('create-user');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function update(User $user, User $model)
+    {
+        return auth()->user()->can('update-user');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function delete(User $user, User $model)
+    {
+        // don't allow to delete administrator user
+        if($user->hasRole('administrator')) {
+            return false;
+        }
+
+        // don't allow to delete current logged in user
+        if($user->id === $model->id) {
+            return false;
+        }
+
+        return auth()->user()->can('delete-user');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function restore(User $user, User $model)
+    {
+        return auth()->user()->can('create-user');
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function forceDelete(User $user, User $model)
+    {
+        return auth()->user()->can('delete-user');
+    }
+}
